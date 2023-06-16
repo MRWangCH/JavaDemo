752f105f0bab88c7acb5b0bd0c035900a84a23d1
wangchuan <chuan.wang@dhc.com.cn>
2023-06-16 11:39:08 +0800

    demo

---
+++ src/test/runnable/Main.java
@@ -0,0 +1,9 @@
+package test.runnable;
+
+public class Main {
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.start();
+    }
+}
